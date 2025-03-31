c29e3ed7e301e1e833ab6db257ce7687e97d62de
juriko123 <jurij.zamarkodelja19@gmail.com>
2025-03-31 16:19:39 +0200

    datoteke

---
+++ Vaje6/Datoteke_rekurzija/Datoteke.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datoteke_rekurzija
+{
+    class Program
+    {
+        static void NajdiImeniki(string trenutniImenik, string datoteka, List<string> iskano)
+        {
+            if (Directory.Exists(trenutniImenik)) // Preverimo, ali imenik obstaja
+            {
+                if (File.Exists(Path.Combine(trenutniImenik, datoteka))) // Preverimo, ali datoteka obstaja
+                {
+                    iskano.Add(trenutniImenik);
+                }
+
+                foreach (string podimenik in Directory.GetDirectories(trenutniImenik)) // Rekurzivno obiščemo podimenike
+                {
+                    NajdiImeniki(podimenik, datoteka, iskano);
+                }
+            }
+        }
+
+            static void Main(string[] args)
+        {
+            string zacetni_imenik = @"C:\Users\Jurij\Desktop\c#\Prog3\Vaje6\";
+            string iskana_dat = "App.config";
+
+            List<string> imeniki = new List<string>();
+
+            NajdiImeniki(zacetni_imenik, iskana_dat, imeniki);
+
+            Console.WriteLine("Imeniki, ki vsebujejo dano datoteko: ");
+            foreach (string imenik in imeniki)
+            {
+                Console.WriteLine(imenik);
+            }
+        }
+    }
+}
---
+++ Vaje6/Datoteke_rekurzija/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Datoteke_rekurzija")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Datoteke_rekurzija")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("409e7320-dacb-4579-9ff7-6b5d0d14d213")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
